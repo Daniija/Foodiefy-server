1fc6119de06a2939a15169575b03a1c3989d9534
Daneil <daneilhyman12@gmail.com>
2022-09-07 16:53:37 -0500

    google tests

--- middleware/fileUpload.js
+++ middleware/fileUpload.js
@@ -21,6 +21,7 @@ const {Storage} = require('@google-cloud/storage');
 const storage1 = new Storage({keyFilename: process.env.KEYFILE});
 const bucket = storage1.bucket(process.env.BUCKET);
 
+Storage.getBuckets().then(x => console.log(x))
 exports.uploadImage = (file) => new Promise((resolve, reject) => {
     const {originalname, buffer} = file
     let fname = originalname.replace(originalname,getTime()+originalname)
